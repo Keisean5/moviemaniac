02f0e323df43c689a15bc3ee44c3d5463028f1c8
Keisean5 <seanksarreal@gmail.com>
2025-08-26 11:50:53 -0700

    Created Added and Styled MovieCard Component

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./MovieList.css";
 import Fire from "../../assets/fire.png";
+import MovieCard from "./MovieCard";
 
 const MovieList = () => {
   return (
@@ -30,7 +31,9 @@ const MovieList = () => {
         </div>
       </header>
 
-      <div className="movie_cards"></div>
+      <div className="movie_cards">
+        <MovieCard />
+      </div>
     </section>
   );
 };
